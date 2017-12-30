74bcbf067c128151119389dd093773d44f09fb04
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2017-12-30 13:23:27 +0300

    Initial Commit

---
+++ pinCode1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pinCode1
+//
+//  Created by ST13 on 30.12.2017.
+//  Copyright © 2017 Kugim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
